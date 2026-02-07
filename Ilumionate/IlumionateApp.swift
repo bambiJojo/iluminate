fb9652a8a295b5ce535926b57f3c2b80426cee4f
lordbyronq <lordbyronq@gmail.com>
2026-02-07 16:24:00 -0700

    Initial Commit

---
+++ Ilumionate/IlumionateApp.swift
@@ -0,0 +1,17 @@
+//
+//  IlumionateApp.swift
+//  Ilumionate
+//
+//  Created by Byron Quine on 2/7/26.
+//
+
+import SwiftUI
+
+@main
+struct IlumionateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IlumionateTests/IlumionateTests.swift
@@ -0,0 +1,17 @@
+//
+//  IlumionateTests.swift
+//  IlumionateTests
+//
+//  Created by Byron Quine on 2/7/26.
+//
+
+import Testing
+@testable import Ilumionate
+
+struct IlumionateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
